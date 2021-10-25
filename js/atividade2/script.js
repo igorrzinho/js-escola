553163f867f97bbd001c54e0fa15787c4bc87a96
igor Silva <82171880+igorrzinho@users.noreply.github.com>
2021-10-25 16:17:11 -0300

    Add files via upload

---
+++ js/atividade2/script.js
@@ -0,0 +1,12 @@
+var num1=parseInt(prompt('digite um numero'));
+var num2=parseInt(prompt('digite um numero'));
+
+var soma = num1 + num2;
+var sub = num1 - num2;
+var mult = num1 * num2;
+var div = num1 / num2;
+
+document.write(`a soma dos dois sao ${soma}<br>`);
+document.write(`a sobtração dos dois sao ${sub}<br>`);
+document.write(`a multiplicação dos dois sao ${mult}<br>`);
+document.write(`a divisão dos dois sao ${div}<br>`);
---
+++ js/atv1/script.js
@@ -0,0 +1,4 @@
+var igor = 'igorrzinho'
+var anos = 15
+var despedida = 'tchau'
+document.write(`ola sou o ${igor} tenho ${anos} e ate mais, ${despedida}`)
